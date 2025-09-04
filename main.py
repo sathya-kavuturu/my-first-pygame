5f8f65242ac792c9ebff4f2c4fb17ebe4268da8d
Sathya Kavuturu <sathya.kavuturu@iyc.ishafoundation.org>
2025-09-04 18:55:10 +0530

    Updated files

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import pygame as pg
 from sys import exit
 
 pg.init()
-screen = pg.display.set_mode((1280,720))
+screen = pg.display.set_mode((800,400))
 pg.display.set_caption("Runner")
 clock = pg.time.Clock()
-test_surface = pg.Surface((100,100))
-test_surface.fill("orange")
+test_font = pg.font.Font("font/Pixeltype.ttf", 50)
+
+sky_surface = pg.image.load("graphics/sky.png").convert()
+ground_surface = pg.image.load("graphics/ground.png").convert()
+text_surface = test_font.render("My Game",False, "Black")
+snail_surface = pg.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_x_pos = 700
 
 while True:
     for event in pg.event.get():
@@ -14,6 +19,13 @@ while True:
             pg.quit()
             exit()
 
-    screen.blit(test_surface,(200,100))
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(350,50))
+    if snail_x_pos == 0:
+        snail_x_pos = 700
+    snail_x_pos -= 4
+    screen.blit(snail_surface, (snail_x_pos,270))
+
     pg.display.update()
     clock.tick(60)
